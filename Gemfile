b84abcb8d59fd4a9876e31afe0332dc6af29c2ca
Uriel Molina <uriel85@gmail.com>
2020-04-27 04:40:03 -0500

    Adds scaffold for users

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'rack-cors'
 
 gem 'pry'
 
+# Use nock for authenticate user using JWT
+gem 'knock'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /api/v1/users
+  # GET /api/v1/users.json
+  def index
+    @api_v1_users = Api::V1::User.all
+  end
+
+  # GET /api/v1/users/1
+  # GET /api/v1/users/1.json
+  def show
+  end
+
+  # POST /api/v1/users
+  # POST /api/v1/users.json
+  def create
+    @user = Api::V1::User.new(api_v1_user_params)
+
+    if @user.save
+      render :show, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/users/1
+  # PATCH/PUT /api/v1/users/1.json
+  def update
+    if @api_v1_user.update(api_v1_user_params)
+      render :show, status: :ok, location: @api_v1_user
+    else
+      render json: @api_v1_user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/users/1
+  # DELETE /api/v1/users/1.json
+  def destroy
+    @api_v1_user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @api_v1_user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def api_v1_user_params
+      params.require(:api_v1_user).permit(:name, :email, :password_digest)
+    end
+end
