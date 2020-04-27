5884b7502b0b92f86f5609439afd7d0109217349
Uriel Molina <uriel85@gmail.com>
2020-04-27 02:22:29 -0500

    Modify filters for admin

--- app/actions/load_activity_log_action.rb
+++ app/actions/load_activity_log_action.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 class LoadActivityLogAction
   def self.execute(params = {})
-    baby_id      = params.fetch(:baby_id, nil)
-    assistant_id = params.fetch(:assistant_id, nil)
-    status       = params.fetch(:status, nil)
+    baby_id      = params.fetch(:baby_id, nil) != '0' ? params.fetch(:baby_id, nil) : nil
+    assistant_id = params.fetch(:assistant_id, nil)  != '0' ? params.fetch(:assistant_id, nil) : nil
+    status       = params.fetch(:status, nil)  != '0' ? params.fetch(:status, nil) : nil
     activity_logs = ActivityLog.activiy_logs_baby(baby_id, assistant_id, status)
     if activity_logs.blank?
       [{"message":{
